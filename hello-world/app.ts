0307de8ecd0bc2b6003e9a12226b95007c98e895
ryuji55 <rt.october.2@icloud.com>
2024-03-17 17:41:06 +0900

    Add:discordのデータ取得

--- hello-world/app.ts
+++ hello-world/app.ts
@@ -1,4 +1,6 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import axios from 'axios';
+import { decycle } from 'json-cyclic';
 
 /**
  *
@@ -10,13 +12,31 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
  *
  */
 
+const API_BASEURL = 'https://discordapp.com/api/';
+const API_TOKEN = 'MTIxODc1NDIwODk3MTYyNDUwOQ.G6r3cB.qgjAPqA0jSX4OdtUXXtEzi45CMW73S8Na8ne2Q';
+
+const fetchDiscord = async () => {
+    return await axios
+        .get(API_BASEURL + 'channels/1149914100084785265/messages', { headers: { Authorization: 'Bot ' + API_TOKEN } })
+        .then((response) => response.data[0])
+        .catch((error) => {
+            console.log(error);
+        });
+};
+
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     try {
+        const message = await fetchDiscord();
+        const content = message.content;
+        const userName = message.author.username;
         return {
             statusCode: 200,
-            body: JSON.stringify({
-                message: 'hello world hoge',
-            }),
+            body: JSON.stringify(
+                decycle({
+                    content,
+                    userName,
+                }),
+            ),
         };
     } catch (err) {
         console.log(err);
