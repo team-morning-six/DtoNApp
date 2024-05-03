6000f921a4043a996b5530123dfdb07913e3aeca
yunosuke924 <y.minegishi0924@gmail.com>
2024-05-03 18:37:09 +0900

    tmp

--- hello-world/app.ts
+++ hello-world/app.ts
@@ -1,17 +1,5 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import axios from 'axios';
 import dayjs from 'dayjs';
-import { decycle } from 'json-cyclic';
-
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- *
- */
 
 const DISCORD_API_BASEURL = 'https://discordapp.com/api/';
 const DISCORD_API_TOKEN = process.env.DiscordApiToken;
